e51eee71fe19f1f07114ee3858630ec03e0f53e2
Akshay Guthal <akguthal@gmail.com>
2019-04-22 00:13:36 -0400

    removed unnecessary  loop thru nodes and added print functions

--- graph.py
+++ graph.py
@@ -8,7 +8,9 @@ class Constraint:
         self.node1 = node1
         self.node2 = node2
         self.constraint_type = constraint_type
-
+    
+    def print_constraint(self):
+        print(self.node1.key, self.constraint_type, self.node2.key)
 
 class ConstraintNetwork:
     def __init__(self, nodes, constraints):
@@ -18,12 +20,23 @@ class ConstraintNetwork:
         self.generate_adjacency_list()
 
     def generate_adjacency_list(self):
-        for node in self.nodes:
-            self.adjacency_list[node.key] = []
-            for constraint in self.constraints:
-                if (constraint.node1.key == node.key) or (constraint.node2.key == node.key):
-                    self.adjacency_list[node.key].append(constraint)
-                    
+        for constraint in self.constraints:
+            if (constraint.node1.key in self.adjacency_list):
+                self.adjacency_list[constraint.node1.key].append(constraint)
+            else:
+                self.adjacency_list[constraint.node1.key] = [constraint]
+            
+            if (constraint.node2.key in self.adjacency_list):
+                self.adjacency_list[constraint.node2.key].append(constraint)
+            else:
+                self.adjacency_list[constraint.node2.key] = [constraint]
+
+    def print_adjacency_list(self):
+        for node in self.adjacency_list:
+            print('Node', node)
+            for constraint in self.adjacency_list[node]:
+                constraint.print_constraint()
+            print('')
 
 
 a = Node('A', [1,2,3,4])
@@ -39,3 +52,4 @@ c4 = Constraint(b, c, '<')
 constraints = [c1, c2, c3, c4]
 
 cn = ConstraintNetwork(nodes, constraints)
+cn.print_adjacency_list()
