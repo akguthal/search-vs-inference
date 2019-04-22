6f7908058e97b29d78fe67cf38faf9f63e6a8466
Nicholas Chiu <knchiu@terpmail.umd.edu>
2019-04-22 16:58:49 -0400

    Added part of check_for_solution in constraint, added some getters

--- graph.py
+++ graph.py
@@ -11,6 +11,20 @@ class Constraint:
     
     def print_constraint(self):
         print(self.node1.key, self.constraint_type, self.node2.key)
+        
+    def check_for_solution(self):
+        # Check that both nodes are initialized
+        if self.node1 is None or self.node2 is None:
+            return False
+        
+        # Check that both domains aren't empty
+        if len(self.node1.values) <= 0 or len(self.node2.values) <= 0:
+            return False
+        
+        # TODO: Check that the a value in one domain matches the constraint relative to the other domain
+        
+        return True
+    
 
 class ConstraintNetwork:
     def __init__(self, nodes, constraints):
@@ -18,6 +32,12 @@ class ConstraintNetwork:
         self.constraints = constraints
         self.adjacency_list = {}
         self.generate_adjacency_list()
+        
+    def get_nodes(self):
+        return self.nodes
+    
+    def get_constraints(self):
+        return self.constraints
 
     def generate_adjacency_list(self):
         for constraint in self.constraints:
@@ -33,7 +53,7 @@ class ConstraintNetwork:
 
     def print_adjacency_list(self):
         for node in self.adjacency_list:
-            print('Node', node)
+            print('Node ' + node)
             for constraint in self.adjacency_list[node]:
                 constraint.print_constraint()
             print('')
@@ -53,3 +73,7 @@ constraints = [c1, c2, c3, c4]
 
 cn = ConstraintNetwork(nodes, constraints)
 cn.print_adjacency_list()
+
+for c in cn.get_constraints():
+#    print(c.values)
+    print(c.check_for_solution())
