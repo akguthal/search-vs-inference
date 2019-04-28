bc8067131cd4dc3983300eedae1a29384b2be66a
Nicholas Chiu <knchiu@terpmail.umd.edu>
2019-04-28 14:30:12 -0400

    Commented AC-1, made more efficient, removed extra print statements

--- graph.py
+++ graph.py
@@ -122,6 +122,7 @@ class ConstraintNetwork:
                     n1val = known[n1]
             
             # There is a known value for both nodes in the constraint
+            # Can this be replaced with self.isAC(n1val, rule, n2val) somehow?
             if (n1val is not None) and (n2val is not None):
                 if rule == '>' and (n1val > n2val):
                     continue
@@ -142,27 +143,22 @@ class ConstraintNetwork:
     
     def isAC(self, n1val, rule, n2val):
         if rule == '>' and (n1val > n2val):
-            print("\t\ttrue")
             return True
         if rule == '<' and (n1val < n2val):
-            print("\t\ttrue")
             return True
         if rule == '<=' and (n1val <= n2val):
-            print("\t\ttrue")
             return True
         if rule == '>=' and (n1val >= n2val):
-            print("\t\ttrue")
             return True
         if rule == '=' and (n1val == n2val):
-            print("\t\ttrue")
             return True
         if rule == '!=' and (n1val != n2val):
-            print("\t\ttrue")
             return True
-        print("\t\tfalse")
         return False
     
-    def reverse_constraint_type(self, rule):
+    # Used to flip constraints around, e.g. A > B becomes B < A
+    # Allows iteration through constraints to check both related nodes
+    def flip_constraint_type(self, rule):
         if rule == '>':
             return '<'
         if rule == '<':
@@ -172,52 +168,69 @@ class ConstraintNetwork:
         if rule == '>=':
             return '<='
         if rule == '=':
-            return '!='
-        if rule == '!=':
             return '='
+        if rule == '!=':
+            return '!='
         
+    # Tests each value in n1's domain for arc consistency with n2's domain
+    # n1's domain is changed, while n2's domain remains the same.
+    # 'operation' refers to the constraint type/rule/whatever you want to call it.
+    def test_for_arc_consistency(self, n1, n2, n1Domain, n2Domain, operation):
+        # Keep a flag to see if anything has changed
+        somethingChanged = False
+        
+        # Looping through each value in domain 1
+        for n1TestVal in n1Domain:
+            # Initializing a temporary variable to remember if this test value is arc-consistent with n2
+            n1Val_AC_n2Val = False
+            
+            # Looping through values in domain 2
+            for n2TestVal in n2Domain:
+                # If the value is arc-consistent
+                if self.isAC(n1TestVal, operation, n2TestVal):
+                    n1Val_AC_n2Val = True # Set the flag to true
+            
+            # If the flag has not been set to true, it must not be arc-consistent with domain 2
+            if not n1Val_AC_n2Val:
+                # Remove the value
+                n1Domain.remove(n1TestVal)
+                somethingChanged = True # And since you removed something, something has changed
+        
+        # Domain 1 has been changed so it is arc-consistent with domain 2.                
+        # Return the flag that something has changed
+        return somethingChanged 
+        
+    # Will probably be renamed to "AC1" or have a switch for which AC algorithm to use
+    # Makes itself (the constraint network) arc-consistent
+    # 'ratio' will be used later to interpolate between search and inference
     def make_arc_consistent(self, ratio):
+        # Flag to see if something has changed (for AC-1)
         somethingChanged = True
-        queue = self.constraints
+        
+        # Keep looping as long as something is changing
         while somethingChanged:
-            somethingChanged = False
-            for con in queue:
+            somethingChanged = False # Assume nothing has changed
+            
+            # Loop over all of the constraints in the network
+            for con in self.constraints:
+                # Setting node 1 and 2 for each constraint
                 n1 = con.node1
                 n2 = con.node2
                 n1Domain = self.nodes[n1]
                 n2Domain = self.nodes[n2]
                 operation = con.constraint_type
-                for n1TestVal in n1Domain:
-                    n1Val_AC_n2Val = False
-                    for n2TestVal in n2Domain:
-                        print("Testing " + str(n1TestVal) + operation + str(n2TestVal))
-                        if self.isAC(n1TestVal, operation, n2TestVal):
-                            n1Val_AC_n2Val = True
-                    if not n1Val_AC_n2Val:
-                        print("Removing " + str(n1TestVal) + " from " + n1)
-                        n1Domain.remove(n1TestVal)
-                        somethingChanged = True
-                    else:
-                        print("Keeping " + str(n1TestVal) + " in " + n1)
-                        
-                n1 = con.node2
-                n2 = con.node1
-                n1Domain = self.nodes[n1]
-                n2Domain = self.nodes[n2]
-                operation = self.reverse_constraint_type(con.constraint_type)
-                for n1TestVal in n1Domain:
-                    n1Val_AC_n2Val = False
-                    for n2TestVal in n2Domain:
-                        print("Testing " + str(n1TestVal) + operation + str(n2TestVal))
-                        if self.isAC(n1TestVal, operation, n2TestVal):
-                            n1Val_AC_n2Val = True
-                    if not n1Val_AC_n2Val:
-                        print("Removing " + str(n1TestVal) + " from " + n1)
-                        n1Domain.remove(n1TestVal)
-                        somethingChanged = True
-                    else:
-                        print("Keeping " + str(n1TestVal) + " in " + n1)
-
+                
+                # Make domain 1 arc-consistent and remember if something has changed
+                somethingChanged1 = self.test_for_arc_consistency(n1, n2, n1Domain, n2Domain, operation)
+                
+                # Make domain 2 arc-consistentand remember if something has changed
+                operation = self.flip_constraint_type(con.constraint_type) # Flipping the operation around
+                somethingChanged2 = self.test_for_arc_consistency(n2, n1, n2Domain, n1Domain, operation)
+            
+            # Check if something has changed in either domain
+            somethingChanged = somethingChanged1 or somethingChanged2
+        
+# nodess['VariableName'] = [List, of, domain, values]
 nodess = {}
 nodess['A'] = [1,2,3,4]
 nodess['B'] = [1,2,3,4]
@@ -229,11 +242,11 @@ c1 = Constraint('A', '>', 'B')
 c2 = Constraint('C', '>', 'B')
 c3 = Constraint('A', '=', 'C')
 c4 = Constraint('A', '<', 'D')
+constraints = [c1, c2, c3, c4]
 
 # Example single constraint
 #c5 = Constraint('A', '=', 3)
-
-constraints = [c1, c2, c3, c4]
+#constraints = [c1, c2, c3, c4, c5]
 
 cn = ConstraintNetwork(nodess, constraints)
 #print(cn.search())
